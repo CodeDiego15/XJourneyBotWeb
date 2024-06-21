451f537aa879a6ff08d02bc54fbc1a4602547649
CodeDiego15 <andrebermudezdiego@gmail.com>
2024-06-21 09:59:36 -0400

    commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,18 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import react from "@astrojs/react";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
   api: {
-    routes: [
-      {
-        src: '/getServerCount',
-        dest: '/api/getServerCount.js',
-      },
-    ],
+    routes: [{
+      src: '/getServerCount',
+      dest: '/api/getServerCount.js'
+    }]
   },
+  output: "server",
+  adapter: vercel()
 });
